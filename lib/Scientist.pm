0c9e8c522aeb1aa5ed5f02b60badea6c93a9a816
Lance Wicks <lw@judocoach.com>
2016-02-28 19:47:57 +0000

    Add basic calling of code.
    Learning started; this code will run both the candidate and control
    code. Enabled works also.
    
    Next is how we get the match/mismatch checking done.

--- lib/Scientist.pm
+++ lib/Scientist.pm
@@ -1,11 +1,44 @@
 unit class Scientist;
 
-has $.experiment;
-has &try;
+has %.context is rw;
+has Bool $.enabled is rw;
+has $.experiment is rw;
+has %!result;
+has &.try is rw;
+has &.use is rw;
 
 method ver { v0.0.1 }
 
-method run {
+method publish {
+    # Requires populating to be useful.
+}
 
-    return 88;
+method run {
+    return &.use.() unless ?$.enabled;
+
+    %!result = (
+        context    => %.context,
+        experiment => $.experiment,
+    );
+
+    my ( $control, $candidate );
+    my $run_control = sub {
+        $control = &.use.();
+    };
+
+    my $run_candidate = sub {
+        $candidate = &.try.();
+    };
+
+    if ( rand > 0.5 ) {
+        $run_control.();
+        $run_candidate.();
+    }
+    else {
+        $run_candidate.();
+        $run_control.();
+    }
+
+    return $control;
+    
 }
--- t/020-basic.t
+++ t/020-basic.t
@@ -5,12 +5,17 @@ use Scientist;
 
 my $test = Scientist.new(
     experiment => 'Tree',
+    enabled => True,
     try => sub {return 99},
+    use => sub {return 88},
 );
-my $name = $test.experiment;
-ok $name eq 'Tree', 'Experiment is set correctly';
+is $test.experiment, 'Tree', 'Experiment is set correctly';
 
-my $result = $test.run();
-ok $result == 88, 'Run() Returns correct value';
+$test.experiment = 'Fauna';
+is $test.experiment, 'Fauna', 'Able to change the experiment name';
+
+
+my $result = $test.run;
+is $result, 88, 'Run() Returns correct value';
 
 done-testing;
